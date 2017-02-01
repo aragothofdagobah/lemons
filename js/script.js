5a131d164c1fc1a6b9f471baf031dad9f18c3e8c
Caleb <aragothofdagobah@gmail.com>
2017-01-31 23:55:17 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function (){
+  $(".lemon").mouseenter(function (){
+	$(".shadow").fadeIn();
+	$(".lemon").transition({y:'-20px'});
+  });
+  $(".lemon").mouseout(function (){
+	$(".shadow").fadeOut();
+	$(".lemon").delay(100).transition({y:'20px'});
+  });
+});
