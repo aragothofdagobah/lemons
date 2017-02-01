d4581b660e640903e9d43eaa018be4c532c6bda6
Caleb <aragothofdagobah@gmail.com>
2017-02-01 18:49:37 -0500

    added icon

--- js/script.js
+++ js/script.js
@@ -7,4 +7,8 @@ $(document).ready(function (){
 	$(".shadow").fadeOut();
 	$(".lemon").delay(100).transition({y:'20px'});
   });
+
+  $(".lemon").click(function (){
+	$("#why").fadeOut();
+  });
 });
