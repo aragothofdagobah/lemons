3458200bd9c66ad1a1b38a473f13ec207ad357c1
Caleb <aragothofdagobah@gmail.com>
2017-02-05 15:59:56 -0500

    Finished Lemon Animation

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+var width = $(window).width();
+var height = $(window).height();
+var clicked = 0;
+var onLeft = false;
 $(document).ready(function (){
   $(".lemon").mouseenter(function (){
 	$(".shadow").fadeIn();
@@ -9,15 +13,41 @@ $(document).ready(function (){
   });
 
   $(".lemon").click(function (){
-	$("#why").fadeOut();
+      displayFactOne(); 
   });
-    
   $(".lemon-slice").click(function (){
-	$("#why2").fadeOut();
-	$("#why3").delay(400).fadeIn();
-	setTimeout( function (){
-	  $(".wrapper").addClass("wrapper-move");
-      $(".lemon-slice").addClass("lemon-rotate");
-	}, 1500); 
+	if(clicked === 0)
+	  displayFactOne();
+	else
+	  displayFact();
   });
 });
+   
+var displayFactOne = function (){
+  $("#why2").fadeOut();
+  $("#why3").delay(400).fadeIn();
+  setTimeout( function (){
+     $(".wrapper").addClass("wrapper-move");
+     $(".lemon-slice").addClass("lemon-rotate");
+     clicked++;
+	}, 1500); 
+
+  $("#buffer").delay(200).fadeIn();
+  $("#fact").delay(2000).fadeIn();
+};
+
+var displayFact = function(){
+  if(onLeft){
+	$(".lemon-slice").addClass("lemon-rotate-right");
+	$(".wrapper").addClass("wrapper-move-right");
+	$(".lemon-slice").removeClass("lemon-rotate-left");
+	$(".wrapper").removeClass("wrapper-move-left");
+  }
+  else{
+    $(".lemon-slice").addClass("lemon-rotate-left");
+    $(".wrapper").addClass("wrapper-move-left");
+	$(".lemon-slice").removeClass("lemon-rotate-right");
+	$(".wrapper").removeClass("wrapper-move-right");
+  }
+  onLeft= !onLeft;
+};
