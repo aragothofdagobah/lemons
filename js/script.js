a47001b2efad969a3c7f19f1b34f4a7e10b6c2f1
Caleb <aragothofdagobah@gmail.com>
2017-02-11 14:39:36 -0500

    added facts

--- js/script.js
+++ js/script.js
@@ -1,7 +1,29 @@
 var facts = [
-  "Fact one", 
-  "Fact two", 
-  "Fact Three"
+  "Lemons are technically berries.", 
+  "Historians believe that lemons have been around since the first century A.D.",
+  "It is believed that they cultivated from the Mediterranean.",
+  "There are three common lemons which are Bearss, Eureka, and Lisbon.",
+  "Lemon trees can produce up to 600 pounds in a year and can grow up to 20 feet tall.",
+  "California and Arizona produce 95% of the entire lemon crop.",
+  "Today, the British Navy requires all ships to carry enough lemons so that every sailor can have an ounce of juice a day.",
+  "An average lemon contains eight seeds.",
+  "An average lemon holds three tablespoons of juice. ",
+  "The juice of a lemon contains 5% of citric acid.",
+  "There are roughly fifteen calories in each lemon.",
+  "Sprinkling the juice on other fruits can prevent them turning brown.",
+  "Lemon juice and hot water is good for a sore throat as it is anti-bacterial.",
+  "Lemons used to be so rare that kings used to present them to each other as gifts.",
+  "During the California Gold Rush in 1849, miners were willing to pay huge amounts of money for a single lemon.",
+  "During the Renaissance, ladies used the juice of a lemon to redden their lips.",
+  "For natural highlights in your hair, apply lemon juice daily, for a week.",
+  "Wealthy Victorians grew lemons trees in their homes as a sign of prestige and to be a fragrant.",
+  "In February and March, Menton in the French Riviera celebrates an annual lemon festival.",
+  "Lemon oil is used in unsealed rosewood fingerboards of stringed instruments.",
+  "They are high in vitamin C.",
+  "They can prevent scurvy.",
+  "To power a flashlight bulb, you need 500 wired lemons to conduct electricity.",
+  "The heaviest lemon was 11 pounds, 9.7 ounces in 2003.",
+  "Lemon was a common unisex name in the 1900’s."
 ];
 var width = $(window).width();
 var height = $(window).height();
@@ -39,7 +61,7 @@ var displayFactOne = function (){
 	}, 1500); 
 
   $("#buffer").delay(200).fadeIn();
-  $("#fact").delay(2000).fadeIn();
+  $("#fact").delay(2400).fadeIn();
 };
 
 var moveLemon = function(){
@@ -60,11 +82,11 @@ var moveLemon = function(){
 };
 
 var displayFact = function(){
-  $("#fact").delay(1300).fadeOut();
+  $("#fact").delay(800).fadeOut();
  setTimeout( function (){  
     $("#fact").empty();
     $("#fact").text(facts[factNum]);
     $("#fact").fadeIn();
     factNum++;
- }, 2000);
+ }, 2700);
 };
